2dd06aba5164c0f1a6ea9326a9ebf91012872434
hivongdc-star <hivongdc@gmail.com>
2026-08-31 20:21:15 +0900

    Reduce race utility to consolidated character shim

--- utils/races.js
+++ utils/races.js
@@ -1,27 +1,2 @@
-module.exports = {
-  nhan: {
-    name: "Nhân",
-    emoji: "👤",
-    gain: { hp: 12, mp: 12, atk: 12, def: 12, spd: 2 }, // cân bằng, SPD ít
-  },
-  ma: {
-    name: "Ma",
-    emoji: "😈",
-    gain: { hp: 6, mp: 6, atk: 26, def: 10, spd: 2 }, // ưu tiên công
-  },
-  tien: {
-    name: "Tiên",
-    emoji: "👼",
-    gain: { hp: 6, mp: 26, atk: 6, def: 10, spd: 2 }, // ưu tiên mana
-  },
-  yeu: {
-    name: "Yêu",
-    emoji: "🦊",
-    gain: { hp: 26, mp: 6, atk: 6, def: 10, spd: 2 }, // ưu tiên máu
-  },
-  than: {
-    name: "Thần",
-    emoji: "⚡",
-    gain: { hp: 12, mp: 12, atk: 12, def: 12, spd: 2 }, // cân bằng (x1.6 breakthrough)
-  },
-};
+// Compatibility shim. Canonical race data lives in commands/character.js.
+module.exports = require("../commands/character").races;

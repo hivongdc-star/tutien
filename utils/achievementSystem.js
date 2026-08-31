225fba1bbef44a6e42488d98538a8903e4cc5e1b
hivongdc-star <hivongdc@gmail.com>
2026-08-31 20:09:21 +0900

    Remove progress achievement compatibility shim

--- utils/achievementSystem.js
+++
@@ -1,8 +0,0 @@
-const progress = require("../commands/progress");
-
-module.exports = {
-  ACHIEVEMENTS: progress.ACHIEVEMENTS,
-  ensureAchv: progress.ensureAchv,
-  recordEvent: progress.recordAchievementEvent,
-  checkUnlocks: progress.checkUnlocks,
-};
